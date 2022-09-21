9d2d739013a3119ad1811eb37fd73a8169d1490d
OlafWindrunner <niebiel1@gmail.com>
2022-09-21 17:41:26 +0200

    uri

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/',
+    uri: 'https://apollo-server-react.herokuapp.com/',
     cache: new InMemoryCache()
 })
 
