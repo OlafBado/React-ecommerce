121427c2b65f277d5188160df4a92f4bcc19eacf
OlafWindrunner <niebiel1@gmail.com>
2022-11-15 19:30:41 +0100

    alt

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,162 +1,191 @@
-import React from 'react'
-import { Markup } from 'interweave';
-import { getDetails } from '../queries/queries';
-import { graphql } from '@apollo/client/react/hoc';
-import { Attributes, Button, Slider } from '.'
+import React from "react";
+import { Markup } from "interweave";
+import { getDetails } from "../queries/queries";
+import { graphql } from "@apollo/client/react/hoc";
+import { Attributes, Button, Slider } from ".";
 
 class ProductDetails extends React.Component {
-
     constructor() {
-        super()
+        super();
         this.state = {
             index: 0,
             selectedOptions: {},
-            setChangedAttributes: []
-        }
-        
+            setChangedAttributes: [],
+        };
     }
 
     componentDidUpdate = (previousProps, previousState) => {
         if (previousProps.data !== this.props.data) {
-            this.setDefaultAttributes(this.props)
+            this.setDefaultAttributes(this.props);
         }
-    }
+    };
 
     variantChange = (e) => {
-        const target = e.target
-        const selectedOptions = this.state.selectedOptions
-        const changedAttribute = selectedOptions.find(element => element.attribute === target.name)
-        changedAttribute.value = target.value
+        const target = e.target;
+        const selectedOptions = this.state.selectedOptions;
+        const changedAttribute = selectedOptions.find(
+            (element) => element.attribute === target.name
+        );
+        changedAttribute.value = target.value;
 
-        const changed = this.state.setChangedAttributes
-        let x = changed.find(item => {
-            let result = item.attribute === changedAttribute.attribute
-            return result
-        })
+        const changed = this.state.setChangedAttributes;
+        let x = changed.find((item) => {
+            let result = item.attribute === changedAttribute.attribute;
+            return result;
+        });
         if (!x) {
-            this.setState(prevState=>({
-                setChangedAttributes: [...prevState.setChangedAttributes, changedAttribute]
-            }))
+            this.setState((prevState) => ({
+                setChangedAttributes: [
+                    ...prevState.setChangedAttributes,
+                    changedAttribute,
+                ],
+            }));
         } else {
-            this.setState(prevState=>({
-                setChangedAttributes: prevState.setChangedAttributes.map(el => el.attribute === x.attribute ? {...el, value: target.value} : el)
-            }))
+            this.setState((prevState) => ({
+                setChangedAttributes: prevState.setChangedAttributes.map((el) =>
+                    el.attribute === x.attribute
+                        ? { ...el, value: target.value }
+                        : el
+                ),
+            }));
         }
 
         this.setState({
-            selectedVariant: changedAttribute
-        })
-    }
+            selectedVariant: changedAttribute,
+        });
+    };
 
     changeImg = (index) => {
         this.setState({
-            index: index
-        })
-    }
+            index: index,
+        });
+    };
 
-    setDefaultAttributes (product) {
-        console.log(product)
+    setDefaultAttributes(product) {
+        console.log(product);
         if (!product.data.loading && product.data.product) {
             if (this.state.setChangedAttributes.length >= 1) {
-
-                const selectedAttributes = product.data.product.attributes.map(p => {
-                    const changedAttribute = this.state.setChangedAttributes.find(o => o.attribute === p.id)
-                    return {
-                        attribute: p.id,
-                        value: changedAttribute ? changedAttribute.value : p.items[0].value
-                    } 
-                } )
-            this.setState({
-                selectedOptions: selectedAttributes
-            })
-
-            } else {
-                const selectedAttributes = product.data.product.attributes.map(p => {
+                const selectedAttributes = product.data.product.attributes.map(
+                    (p) => {
+                        const changedAttribute =
+                            this.state.setChangedAttributes.find(
+                                (o) => o.attribute === p.id
+                            );
                         return {
                             attribute: p.id,
-                            value: p.items[0].value
-                        }                
-                } )
+                            value: changedAttribute
+                                ? changedAttribute.value
+                                : p.items[0].value,
+                        };
+                    }
+                );
                 this.setState({
-                    selectedOptions: selectedAttributes
-                })
+                    selectedOptions: selectedAttributes,
+                });
+            } else {
+                const selectedAttributes = product.data.product.attributes.map(
+                    (p) => {
+                        return {
+                            attribute: p.id,
+                            value: p.items[0].value,
+                        };
+                    }
+                );
+                this.setState({
+                    selectedOptions: selectedAttributes,
+                });
             }
         }
     }
 
     displayDetails = () => {
-        const { index } = this.state
-        const { product } = this.props.data
-        const { currency } = this.props
+        const { index } = this.state;
+        const { product } = this.props.data;
+        const { currency } = this.props;
         if (product) {
             return (
                 <>
-                    <div className='product-details-images'>
+                    <div className="product-details-images">
                         {product.gallery.map((img, index) => {
                             return (
-                                <img onClick={() => this.changeImg(index)} key={index} src={img} />
-                            )
+                                <img
+                                    onClick={() => this.changeImg(index)}
+                                    key={index}
+                                    src={img}
+                                    alt="product"
+                                />
+                            );
                         })}
                     </div>
-                    <div className='product-details-main'>
-                        <div className='details-slider'>
-                            <img className='product-details-image' src={product.gallery[index]}/>
-                            {product.gallery.length > 1 
-                            ?
-                            <Slider 
-                                index={index}
-                                changeImg={this.changeImg}
-                                galleryLength={product.gallery.length}
-                                place='details'
+                    <div className="product-details-main">
+                        <div className="details-slider">
+                            <img
+                                alt="product"
+                                className="product-details-image"
+                                src={product.gallery[index]}
                             />
-                            :
-                            null}
+                            {product.gallery.length > 1 ? (
+                                <Slider
+                                    index={index}
+                                    changeImg={this.changeImg}
+                                    galleryLength={product.gallery.length}
+                                    place="details"
+                                />
+                            ) : null}
                         </div>
-                        <div className='product-details-details'>
-                            <p className='details-brand'>{product.brand}</p>
-                            <p className='details-name'>{product.name}</p>
+                        <div className="product-details-details">
+                            <p className="details-brand">{product.brand}</p>
+                            <p className="details-name">{product.name}</p>
                             <Attributes
                                 selectedOptions={this.state.selectedOptions}
                                 attributes={product.attributes}
                                 variantChange={this.variantChange}
-                                place={'details'}
+                                place={"details"}
                             />
-                            <p className='details-price'>PRICE:</p>
-                            <p className='details-amount'>{this.props.currency}{this.props.findAmount(product, currency)}</p>
+                            <p className="details-price">PRICE:</p>
+                            <p className="details-amount">
+                                {this.props.currency}
+                                {this.props.findAmount(product, currency)}
+                            </p>
                             <Button
                                 disabled={product.inStock ? "" : true}
-                                onClick={() => this.props.handlerAddToCart({...product, selectedOptions : this.state.selectedOptions})}
-                                weight={'600'}
-                                size={'16px'}
-                                color={product.inStock ? '#FFFFFF' : '#8D8F9A'}
-                                border={'none'}
-                                width={'100%'}
-                                height={'52px'}
-                                bg={product.inStock ? 'rgba(94, 206, 123, 1)' : '#eeeeee'}
+                                onClick={() =>
+                                    this.props.handlerAddToCart({
+                                        ...product,
+                                        selectedOptions:
+                                            this.state.selectedOptions,
+                                    })
+                                }
+                                weight={"600"}
+                                size={"16px"}
+                                color={product.inStock ? "#FFFFFF" : "#8D8F9A"}
+                                border={"none"}
+                                width={"100%"}
+                                height={"52px"}
+                                bg={
+                                    product.inStock
+                                        ? "rgba(94, 206, 123, 1)"
+                                        : "#eeeeee"
+                                }
                             >
-                                {product.inStock ? 'ADD TO CART' : 'OUT OF STOCK'}
+                                {product.inStock
+                                    ? "ADD TO CART"
+                                    : "OUT OF STOCK"}
                             </Button>
-                            <div className='product-details-description'>
-                                <Markup content={product.description}/>
+                            <div className="product-details-description">
+                                <Markup content={product.description} />
                             </div>
                         </div>
                     </div>
                 </>
-            )
+            );
         } else {
-            return (
-                <div>Product not found...</div>
-            )
+            return <div>Product not found...</div>;
         }
-    }
-    
+    };
+
     render() {
-        return (
-            <div className='product-details'>
-                {this.displayDetails()}
-            </div>
-            
-        )
+        return <div className="product-details">{this.displayDetails()}</div>;
     }
 }
 
@@ -164,9 +193,9 @@ export default graphql(getDetails, {
     options: (props) => {
         return {
             variables: {
-                id: window.location.pathname.split('/')[1]
+                id: window.location.pathname.split("/")[1],
             },
-            fetchPolicy: "no-cache"
-        }
-    }
-})(ProductDetails)
+            fetchPolicy: "no-cache",
+        };
+    },
+})(ProductDetails);
